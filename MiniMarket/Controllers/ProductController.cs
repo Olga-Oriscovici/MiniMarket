e95e6a92e866ddf7031316e2d9619366e3a0b3e3
Оля <@DESKTOP-GFFKSRH>
2025-05-23 00:22:13 +0300

    Add DbContext

---
+++ MiniMarket/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniMarket.Data;
+
+namespace MiniMarket.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductController (ApplicationDbContext _context)
+        {
+            _context = _context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MiniMarket/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniMarket.Models;
+
+namespace MiniMarket.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        
+
+        public DbSet<Product> products { get; set; }
+    }
+}
